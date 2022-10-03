a021b684620b64dfdd1be71e04a547896594e383
Jordi272 <74501189+Jordi272@users.noreply.github.com>
2022-10-03 11:57:24 +0200

    Add files via upload

---
+++ web/src/index.php
@@ -0,0 +1,23 @@
+<?php
+    include('../conexion.php');
+
+?>
+
+<!DOCTYPE html>
+<html lang="es">
+<head>
+  <meta charset="utf-8">
+    <title>HTML</title>
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="estilo.css">
+        </head>
+        <h2>Los datos son</h2>
+
+        <?php
+            include('../mostrarDato.php');
+        ?>
+    <body>
+  
+    </body>
+</html>
+
